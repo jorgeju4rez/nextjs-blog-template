02c9c3cb66e01bbc69f0c69e94a0e151e29ae8ab
jorgeju4rez <emprendojuarez@gmail.com>
2023-01-31 21:00:47 +0100

    add: tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>hello world</h1>
     </main>
   );
